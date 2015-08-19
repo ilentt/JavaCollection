314d1f7cca7a7132fdfb7c47e7b212c2f28d40e0
ilentt <ilentt@ilenlab.com>
2015-08-19 14:26:56 +0700

    update duplicated list

--- JavaCollection/src/ilentt/ilenlab/com/javacollection/DuplicatedList.java
+++ JavaCollection/src/ilentt/ilenlab/com/javacollection/DuplicatedList.java
@@ -3,6 +3,7 @@ package ilentt.ilenlab.com.javacollection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class DuplicatedList {
@@ -23,10 +24,21 @@ public class DuplicatedList {
 		System.out.println("Element list: " +listWithOutDuplicated.toString());
 	}
 	
+	public void RemoveDuplicatedItemv2() {
+		System.out.println("Size of duplicated list: " + this.dupList.size());
+		System.out.println("Element list: " +this.dupList.toString());
+		
+		LinkedHashSet<String> listToSet = new LinkedHashSet<String>(this.dupList);
+		List<String> listWithOutDuplicated = new ArrayList<String>(listToSet);
+		
+		System.out.println("\nSize of duplicated list after remove duplicate element: " + listWithOutDuplicated.size());
+		System.out.println("Element list: " +listWithOutDuplicated.toString());
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DuplicatedList d = new DuplicatedList();
-		d.RemoveDuplicatedItem();
+		//d.RemoveDuplicatedItem();
+		d.RemoveDuplicatedItemv2();
 	}
 
 }
